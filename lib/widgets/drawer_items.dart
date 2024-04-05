baabe438655b8b174856d6708a7828d09c5c7aa8
Sara-Salem-FCI <sara2004sal@gmail.com>
2024-04-05 04:44:49 +0200

    Edit dashboard text and fit it

--- lib/widgets/drawer_items.dart
+++ lib/widgets/drawer_items.dart
@@ -4,12 +4,14 @@ import '../models/drawer_model.dart';
 
 class DrawerItemsListView extends StatelessWidget {
   const DrawerItemsListView({super.key});
+
   static List<DrawerModel> drawerItems = [
-    DrawerModel(title: 'Dashboard', icon: Icons.dashboard),
-    DrawerModel(title: 'Favorite', icon: Icons.favorite),
-    DrawerModel(title: 'Settings', icon: Icons.settings),
-    DrawerModel(title: 'Help', icon: Icons.help),
+    DrawerModel(title: 'D a s h b o a r d', icon: Icons.dashboard),
+    DrawerModel(title: 'F a v o r i t e', icon: Icons.favorite),
+    DrawerModel(title: 'S e t t i n g s', icon: Icons.settings),
+    DrawerModel(title: 'H e l p', icon: Icons.help),
   ];
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -18,11 +20,16 @@ class DrawerItemsListView extends StatelessWidget {
       itemBuilder: (BuildContext context, int index) {
         return ListTile(
           leading: Icon(drawerItems[index].icon),
-          title: Text(drawerItems[index].title),
+          title: FittedBox(
+            fit: BoxFit.scaleDown,
+            alignment: Alignment.centerLeft,
+            child: Text(
+              drawerItems[index].title.toUpperCase(),
+            ),
+          ),
         );
       },
       itemCount: drawerItems.length,
     );
   }
 }
-
